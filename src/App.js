959df46b990a142265b6903221dfb672485bf9c9
siva vempuluru <vempulurusiva@gmail.com>
2024-07-19 21:00:08 +0530

    progress

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import SquareComponent from './React js/orderlist/list';
-import ConditionalRendering from './React js/conditional/conditional';
-import Counter from './React js/conditional/countor';
+// import SquareComponent from './React js/orderlist/list';
+// import ConditionalRendering from './React js/conditional/conditional';
+// import Counter from './React js/conditional/countor';
 
+import IplTeams from './React js/conditional/iplTeam';
 
 
 const App = () =>{
  return (
   <div>
-  <ConditionalRendering/>
+
+    <IplTeams/>
+  {/* <ConditionalRendering/> */}
 
   <br/>
-  <SquareComponent/>
+  {/* <SquareComponent/> */}
  <br/>
-   <Counter/>
+   {/* <Counter/> */}
 
   
      
